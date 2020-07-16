82330c120604d5e225a209f530e52b197ec17313
Renato Guadamuz <renato.guadamuz@gmail.com>
2020-07-16 16:08:25 -0400

    Update Start_RStudio.R

--- Start_RStudio.R
+++ Start_RStudio.R
@@ -1,14 +1,19 @@
 #Reset environment
-rm(list=ls())
-options(scipen = 4)
+rm(list=ls()) #remove all variables
+gc() #garbage collection
+tryCatch({dev.off()}, error = function(e) {}) #clear plots
+options(scipen=3, digits=3) #scientific notation and 3 digits
+cat("\014") #clear console
 
-#Packages
-{packages <- c("readxl","WVPlots","ggplot2","car","corrplot","MASS","PerformanceAnalytics")
+#Install/load Packages
+{packages <- c("package1","package2","package3")
   install.pckg <- packages[!packages %in% installed.packages()]
   for(i in install.pckg) install.packages(i,dependencies=TRUE)
   suppressMessages(sapply(packages,require,character=TRUE))
   rm(packages, install.pckg, i)}
 
-#Get database
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) #Directory to current file
+#Set directory
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) #Directory to current file path
 setwd('..') #Working directory to parent folder
+
+#Read file here and start analyses
